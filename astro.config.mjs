5d687a00d2c90c2d3977c570d730760f5c4c73b9
SWAT♤ <139399294+Hector-SWAT@users.noreply.github.com>
2025-08-29 21:59:23 -0600

    Actualización de astro.config.mjs y otros cambiosv2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import robots from '@astrojs/robots';
 
 export default defineConfig({
   site: 'https://taxiayuujk.com', // 👈 tu dominio (IMPORTANTE)
   integrations: [
     tailwind(),
     sitemap(),
-    robots({
-      sitemap: 'https://taxiayuujk.com/sitemap-index.xml',
-    }),
   ],
 });
